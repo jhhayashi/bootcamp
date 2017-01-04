4fb93161f02d6e00eb0fe8be8559ef0aa5aee3c8
Jordan Hayashi <jhhayashi@gmail.com>
2017-01-04 11:37:06 -0500

    Add exercise for day1 morning

---
+++ day1/morning/exercises/app.js
@@ -0,0 +1,37 @@
+// try running this code with `node app.js`, and you'll notice it errors.
+// what must you do to make the code work?
+
+var express = require('express')
+var app = express()
+
+// use a for loop to create an array with each lowercase letter in the alphabet
+// resulting array should be ['a', 'b', ... 'y', 'z']
+
+// use a for loop to generate an app.get function for each endpoint
+// callback function should res.send the letter's index in the alphabet
+// do not use block scoped variables (let)
+// do not use Array.prototype.forEach()
+// first endpoint should be:
+// app.get('/a', function(req, res) { res.send("1") });
+
+
+
+
+
+
+// by the time you get to this point, jordan will have pushed a new commit
+// updating ./students.js
+// import that array into this file, and write an endpoint at GET /partners
+// that randomly pairs students. feel free to use whatever data structure you
+// see fit
+
+
+
+
+app.get('/', function (req, res) {
+    res.send('Hello, World!')
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+});
