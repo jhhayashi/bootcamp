856c2e3a2318a622cb2dbf74a0de3836c3a6f555
Jordan Hayashi <jhhayashi@gmail.com>
2017-01-04 11:45:49 -0500

    Add students to array

--- day1/morning/exercises/students.js
+++ day1/morning/exercises/students.js
@@ -1,1 +1,3 @@
-[]
+exports = ['Catherine', 'Joey', 'Shira', 'Jeffery', 'Alex', 'Hailey', 'Hoyoung', 'Galen',
+    'Jeremy', 'Allison', 'Akshitha', 'Daniel', 'Ivraj', 'Tiffany', 'Josh', 'Dong',
+    'Will', 'Brandon', 'Ethan', 'Bardi', 'Jason', 'Samir', 'Gerardo']
