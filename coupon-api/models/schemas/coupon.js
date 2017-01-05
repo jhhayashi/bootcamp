762358ae6b78717933b4dde049668332b04a5409
Jordan Hayashi <jhhayashi@gmail.com>
2017-01-05 13:45:06 -0500

    Add required info to objects

--- coupon-api/models/schemas/coupon.js
+++ coupon-api/models/schemas/coupon.js
@@ -1,13 +1,13 @@
-name: {type: String, require: true, trim: true},
-url: {type: String, required: true, trim: true},
-companyName: {type: String, required: true, trim: true},
-startDate: {type: Date, default: Date.now, index: true},
-endDate: {type: Date, index: true},
-tags: [Number],
-clicks: {type: [Date], default: []},
-views: {type: [Date], default: []},
-redeemed: {type: [Date], default: []},
-postedBy: {type: Schema.ObjectId, ref: 'User', required: true},
-approvedDate: Date,
-createdAt: 'createdDate',
-updatedAt: 'updatedDate'
+name
+url
+companyName
+startDate
+endDate
+tags
+clicks
+views
+redeems
+postedBy
+approvedDate
+createdDate
+updatedDate
