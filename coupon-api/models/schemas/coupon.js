5a94479783ca8ba9440e6851c7312dbf5f97744e
Jordan Hayashi <jhhayashi@gmail.com>
2017-01-05 13:55:58 -0500

    Add boilerplate schema code

--- coupon-api/models/schemas/coupon.js
+++ coupon-api/models/schemas/coupon.js
@@ -1,13 +1,26 @@
-name
-url
-companyName
-startDate
-endDate
-tags
-clicks
-views
-redeems
-postedBy
-approvedDate
-createdDate
-updatedDate
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var couponSchema = new Schema({
+    // define schema here
+    
+    },
+    {
+        toObject: { getters: true },
+        // change name of mongoose default timestamps
+        timeStamps: {
+            createdAt: 'createdDate',
+            updatedAt: 'updatedDate'
+        }
+    }
+);
+
+couponSchema.pre('save', function(callback) {
+    // run hook code
+    
+    callback();
+});
+
+var Coupon = mongoose.model('Coupon', couponSchema);
+
+module.exports = Coupon;
--- coupon-api/models/schemas/user.js
+++ coupon-api/models/schemas/user.js
@@ -1,16 +1,32 @@
-firstName
-lastName
-address
-classYear
-email
-phone
-phoneProvider
-venmo
-isAdmin
-isSuperAdmin
-hash
-companyName
-createdDate
-updatedDate
-interests
-timeSpent
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    // define schema here
+    
+    },
+    {
+        toObject: { getters: true },
+        // change name of mongoose default timestamps
+        timeStamps: {
+            createdAt: 'createdDate',
+            updatedAt: 'updatedDate'
+        }
+    }
+);
+
+userSchema.pre('save', function(callback) {
+    // run hook code
+    
+    callback();
+});
+
+
+// create any methods
+userSchema.methods.greet = function() {
+    console.log('hi!');
+};
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = Coupon;
