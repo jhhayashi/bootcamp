bb672998c426b0c511bbc93c4fb9e5cfc9683c05
Jordan Hayashi <jhhayashi@gmail.com>
2017-01-05 15:48:16 -0500

    Add user schema as written in class

--- coupon-api/models/schemas/user.js
+++ coupon-api/models/schemas/user.js
@@ -2,8 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-    // define schema here
-    
+        // define schema here
+        firstName: String,
+        lastName: String,
+        address: String,
+        classYear: Number,
+        email: String,
+        phone: String,
+        phoneProvider: String,
+        isAdmin: Boolean,
+        isSuperAdmin: Boolean,
+        hash: String,
+        companyName: String,
+        interests: [String],
+        timeSpent: Number
     },
     {
         toObject: { getters: true },
@@ -17,15 +29,44 @@ var userSchema = new Schema({
 
 userSchema.pre('save', function(callback) {
     // run hook code
-    
+    if (this.isAdmin) {
+        if (!this.hash && !this.password) {
+            throw new Error('Missing password');
+        }
+        this.hash = this.hash || this.password;
+        
+        // TODO hash the pw
+        
+
+    } 
+    // non-admins
+    else {
+        if (!this.phone) {
+            throw new Error('Missing phone');
+        }
+
+        // TODO ensure phone number is actually legit
+        
+
+        if (!this.phoneProvider) {
+            throw new Error('Missing provider');
+        }
+        
+
+    }
     callback();
 });
 
 
 // create any methods
 userSchema.methods.greet = function() {
-    console.log('hi!');
+    console.log('hi!' + this.firstName);
 };
+
+// TODO method to check hashed password
+userSchema.methods.checkPassword = function(pw) {
+    return (this.hash === pw);
+}
 
 var User = mongoose.model('User', userSchema);
 
