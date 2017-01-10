cbb23cbd1114bf978414dc2693ec2a08221a05e6
Jordan Hayashi <jhhayashi@gmail.com>
2017-01-10 00:38:53 -0500

    Add exercise for tuesday morning

---
+++ day5/morning/exercise/app/models/students.js
@@ -0,0 +1,3 @@
+module.exports = ['Catherine', 'Joey', 'Shira', 'Jeffery', 'Alex', 'Hailey', 'Hoyoung', 'Galen',
+    'Jeremy', 'Allison', 'Akshitha', 'Daniel', 'Ivraj', 'Tiffany', 'Josh', 'Dong',
+    'Will', 'Brandon', 'Ethan', 'Bardi', 'Jason', 'Samir', 'Gerardo', 'Chiyoung']
