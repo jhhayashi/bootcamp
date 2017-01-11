5e5f553e91d0a9f33bd98332c6e3bb11dd3cd000
Jordan Hayashi <jhhayashi@gmail.com>
2017-01-11 13:42:11 -0500

    Add auth to routes and update config file

--- coupon-api/coupon-api.js
+++ coupon-api/coupon-api.js
@@ -1,37 +1,43 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 
 const config = require('./models/config');
 
 const users = require('./controllers/users');
-const admins = require('./controllers/admins');
 const coupons = require('./controllers/coupons');
+const admins = require('./controllers/admins');
+const sender = require('./controllers/sender');
 const auth = require('./controllers/auth');
 
-var app = express();
-
-// mongoose.connect('localhost:5000');
+// http://mongoosejs.com/docs/promises.html
+mongoose.Promise = global.Promise;
 mongoose.connect(config.dbUrl, {server: {socketOptions: {keepAlive: 120}}});
 
-if (app.get('env') === 'development') var dev = true;
+var app = express();
+var router = express.Router();
 
 // log if in dev mode
-if (dev) app.use(logger('dev'));
+if (app.get('env') !== 'production') app.use(logger('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-// ==========================================
-// More Middleware
-// ==========================================
-
-app.param('id', function(req, res, next, id) {
+//================================================
+// Middleware
+//================================================
+router.param('id', (req, res, next, id) => {
     if (!id.match(/^[0-9a-fA-F]{24}$/))
         return res.status(400).send('Invalid ID');
+    next();
+});
+
+router.param('phone', (req, res, next, phone) => {
+    if (!(+phone) || phone.length !== 10)
+        return res.status(400).send('Invalid phone');
     next();
 });
 
@@ -39,41 +45,59 @@ app.param('id', function(req, res, next, id) {
 // Routes
 //================================================
 
-app.get('/users', users.getUsers);
-app.get('/users/:id', users.getUserById);
-app.post('/users', users.createUser);
-app.put('/users/:id', users.updateUser);
-app.delete('/users/:id', users.deleteUserById);
+router.route('/users')
+    .get(auth.superAdminRequired, users.getAllUsers)
+    .post(users.createUser);
+router.route('/users/send')
+    .post(auth.superAdminRequired, sender.sendAllCouponsToAllUsers);
+router.route('/users/:id')
+    .get(auth.superAdminRequired, users.getUserById)
+    .put(users.updateUser)
+    .delete(users.deleteUserById);
+router.route('/users/phone/:phone')
+    .delete(users.deleteUserByPhone);
 
-app.post('/admins', admins.createAdmin);
+router.route('/coupons')
+    .get(coupons.getActiveCoupons)
+    .post(auth.adminRequired, coupons.createCoupon);
+router.route('/coupons/send')
+    .post(auth.superAdminRequired, sender.sendAllCouponsToAllUsers);
+router.route('/coupons/:id')
+    .get(coupons.getCouponById)
+    .post(auth.superAdminRequired, coupons.approveCoupon)
+    .put(auth.adminRequired, coupons.updateCoupon)
+    .delete(auth.adminRequired, coupons.deleteCouponById);
+router.route('/coupons/:id/send')
+    .post(auth.superAdminRequired, sender.sendCouponById);
 
-app.get('/coupons', coupons.getAllCoupons);
-app.get('/coupons/active', coupons.getActiveCoupons);
-app.get('/coupons/unapproved', coupons.getUnapprovedCoupons);
-app.put('/coupons/:id/approve', auth.superAdminRequired, coupons.approveCoupon);
-app.get('/coupons/:id', coupons.getCouponById);
-app.post('/coupons', coupons.createCoupon);
-app.put('/coupons/:id', coupons.updateCoupon);
-app.delete('/coupons/:id', coupons.deleteCouponById);
+router.route('/admins')
+    .post(auth.superAdminRequired, admins.createAdmin);
+router.route('/admins/coupons')
+    .get(auth.superAdminRequired, coupons.getUnapprovedCoupons);
 
-app.post('/auth/token', auth.loginUser);
+
+router.route('/auth/token')
+    .post(auth.loginUser);
+
+app.use('/', router);
 
 // handle 404
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
-    // who knows
     err.status = 404;
     next(err);
 });
 
-// another errorhandler
-app.use(function(err, req, res, next) {
-    console.log('oops');
-    next(err);
-});
+// development error handler
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        console.log(err);
+        res.status(err.status || 500).send();
+    });
+}
 
+// production error handler
 app.use(function(err, req, res, next) {
-    console.log(err);
     res.status(err.status || 500).send();
 });
 
--- coupon-api/models/config.js
+++ coupon-api/models/config.js
@@ -1,9 +1,65 @@
 module.exports = {
     port: 3000,
     dbUrl: 'localhost:5000',
-
-    // secret for hashing tokens
+    
+    // secret for creating tokens
     secret: 'ThisIsDefinitelyNotTheSecret',
 
+    // mail options, also uses for init script to create first superAdmin
+    // Gmail has a limit of 500 recipients a day
+    emailFromName: 'HSA Coupons',
+    emailFromAddress: 'hsacoupons@gmail.com',
+    emailPassword: 'couponswoo',
 
+
+    providers: [
+        {
+            name: 'altel',
+            sms: '@sms.alltelwireless.com',
+            mms: '@mms.alltelwireless.com',
+            longName: 'Altel'
+        },
+        {
+            name: 'att',
+            sms: '@txt.att.net',
+            mms: '@mms.att.net',
+            longName: 'AT&T'
+        },
+        {
+            name: 'boost',
+            sms: '@sms.myboostmobile.com',
+            mms: '@myboostmobile.com',
+            longName: 'Boost Mobile'
+        },
+        {
+            name: 'sprint',
+            sms: '@messaging.sprintpcs.com',
+            mms: '@pm.sprint.com',
+            longName: 'Sprint'
+        },
+        {
+            name: 'tmobile',
+            sms: '@tmomail.net',
+            mms: '@tmomail.net',
+            longName: 'T-Mobile'
+        },
+        {
+            name: 'uscellular',
+            sms: '@email.uscc.net',
+            mms: '@mms.uscc.net',
+            longName: 'U.S. Cellular'
+        },
+        {
+            name: 'verizon',
+            sms: '@vtext.com',
+            mms: '@vzwpix.com',
+            longName: 'Verizon'
+        },
+        {
+            name: 'virgin',
+            sms: '@vmobl.com',
+            mms: '@vmpix.com',
+            longName: 'Virgin Mobile'
+        }
+    ]
 };
